f4f7422bdbac7c323843dcca8d66833542a66b89
bpb2008 <brittanythompson08@gmail.com>
2023-11-20 16:06:46 -0600

    fixed css and moved quotes object into it's own file

--- src/components/QuoteText.jsx
+++ src/components/QuoteText.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-const QuoteText = (props) => (
-  <div>
-    <p>{props.quote.text}</p>
-    <h3>{props.quote.speaker}</h3>
-    <h4>{props.quote.movie}</h4>
-  </div>
-);
+const QuoteText = (props) => {
+  const { text, speaker, movie } = props.quote;
+  return (
+    <div>
+      <p>{text}</p>
+      <h3>{speaker}</h3>
+      <h4>{movie}</h4>
+    </div>
+  );
+};
 
 export default QuoteText;
